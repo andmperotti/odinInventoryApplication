9f19d17eda1a5e5c55113ba15c3a9a3a36ec9363
Andrew Perotti <Perotti28@gmail.com>
2026-03-18 12:41:16 -0700

    feat: add a populate database file
    This file can be invoked to create schema, and tables in our database, and even add items to those tables just created.

---
+++ db/populateDb.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+
+const SQL = `
+CREATE SCHEMA IF NOT EXISTS public;
+CREATE TABLE IF NOT EXISTS public.categories (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT 
+);
+
+INSERT INTO categories (name) VALUES ('shorts');
+
+CREATE TABLE IF NOT EXISTS public.items (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name TEXT, quantity INTEGER, price INTEGER, categoryID INTEGER);
+
+INSERT INTO items (name, quantity, price, categoryID) VALUES ('wubby', 8, 100, 1);
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.local_connection_string,
+  });
+  //if you wanted to pass the db location when executing this script then you would access that argument to the script in the above line using process.argv
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
