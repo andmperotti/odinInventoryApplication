f421a32154030155d62d6de60bed6aadaa69919d
Andrew Perotti <Perotti28@gmail.com>
2026-03-18 10:38:06 -0700

    feat: add routes for endpoint categories

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("node:path");
+const { indexRouter } = require("./routes/indexRouter");
+const { categoryRouter } = require("./routes/categoryRouter");
+const { itemRouter } = require("./routes/itemRouter");
 
 //set the view engine and define the views location
 app.set("views", path.join(__dirname, "views"));
@@ -10,8 +13,8 @@ app.set("view engine", "ejs");
 //middleware to parse form input entry data into the request
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("hi");
-});
+app.use("/", indexRouter);
+app.use("/category", categoryRouter);
+app.use("/category/:categoryId/items", itemRouter);
 
 app.listen(3002);
