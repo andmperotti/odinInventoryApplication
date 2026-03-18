96a40cb7490a0825d543a0c2c6e8e8c3d19f509e
Andrew Perotti <Perotti28@gmail.com>
2026-03-18 12:35:10 -0700

    build: import db queries file
    feat: add root route handler
    
    This handler invokes a query function from the previously mentioned dbQueries file which obtains the category records and responds by sending its results to a view.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const indexRouter = Router();
+const dbQueries = require("../db/queries");
 
-indexRouter.get("/", (req, res) => {
-  //test return
-  res.send("hihi");
+indexRouter.get("/", async (req, res) => {
   //generate links for each category in the db
+  //so query db for categories, also obtaining their id in the table as this will be used to access specific category pages
+  let categories = await dbQueries.getCategories();
+  res.render("homepage", { categories });
 });
 module.exports = { indexRouter };
