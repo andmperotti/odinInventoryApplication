28d25bc56f287323164541dd527868f8d0e6d3e8
Andrew Perotti <Perotti28@gmail.com>
2026-03-19 12:00:02 -0700

    fix: change var name for category name
    Was confusing before as 'category' which could have been category name or id

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,9 +6,9 @@ categoryRouter.get("/:categoryId", async (req, res) => {
   //items refers to items in the database which have a categoryID equal to that of the parent category clicked on
   let items = await dbQueries.getItems(req.params.categoryId);
   //to state the name of the category we use a small query in the url to carry it from the root route of the application to this category page. This is a band aid as I'm sure there is a better way to do this
-  let category = req.query.category;
+  let categoryName = req.query.category;
   //respond with the category view and passing it data from the db query and reroute the query parameter value
-  res.render("category", { items, category });
+  res.render("category", { items, categoryName });
 });
 
 module.exports = { categoryRouter };
