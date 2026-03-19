d70b25432188bbac9f8189e6ad1198f951b4b64d
Andrew Perotti <Perotti28@gmail.com>
2026-03-19 12:00:56 -0700

    feat: add route handler for specific item routes

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -1,4 +1,11 @@
 const { Router } = require("express");
 const itemRouter = Router();
+const dbQueries = require("../db/queries");
+
+itemRouter.get("/:itemId", async (req, res) => {
+  let item = await dbQueries.getItem(req.params.itemId);
+  let categoryName = req.query.category;
+  res.render("item", { item, categoryName });
+});
 
 module.exports = { itemRouter };
