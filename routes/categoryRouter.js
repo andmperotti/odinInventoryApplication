f266f8ab6aa64067049795c785d3d111d7398a9a
Andrew Perotti <Perotti28@gmail.com>
2026-03-19 10:34:46 -0700

    feat: add route for category
    This route handler function not only invokes a db query method but also notes a url parameter which will be used on the view to display what category is being viewed on the rendered category page. I'm sure there's a better way to do that but that's for a future commit.

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,4 +1,14 @@
 const { Router } = require("express");
 const categoryRouter = Router();
+const dbQueries = require("../db/queries");
+
+categoryRouter.get("/:categoryId", async (req, res) => {
+  //items refers to items in the database which have a categoryID equal to that of the parent category clicked on
+  let items = await dbQueries.getItems(req.params.categoryId);
+  //to state the name of the category we use a small query in the url to carry it from the root route of the application to this category page. This is a band aid as I'm sure there is a better way to do this
+  let category = req.query.category;
+  //respond with the category view and passing it data from the db query and reroute the query parameter value
+  res.render("category", { items, category });
+});
 
 module.exports = { categoryRouter };
