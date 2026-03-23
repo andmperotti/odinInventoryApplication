5cc04a2aafb91a11055d14c893b32e99eaf82cf5
Andrew Perotti <Perotti28@gmail.com>
2026-03-23 12:11:58 -0700

    fix: change getItems
    This method was changed as database records were changed

---
+++ controllers/itemController.js
@@ -0,0 +1,14 @@
+const dbQueries = require("../db/queries");
+
+async function getItems(categoryId) {
+  let items = await dbQueries.getItems(categoryId);
+  let categoryName = dbQueries.getCategoryName(categoryId);
+  res.render("items", { items, categoryId, categoryName });
+}
+
+// async function createItem(name, quantity, price, categoryIds) {
+//   //attempt item creation
+//   //if successful respond with new item view
+//   //otherwise respond with a statement on why it wasn't made
+// }
+module.exports = { getItems };
