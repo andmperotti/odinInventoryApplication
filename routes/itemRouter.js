aa721f76dffc9d9a1c714d6994b2d30d160e1352
Andrew Perotti <Perotti28@gmail.com>
2026-03-25 12:13:18 -0700

    feat: add update item route
    This route will be used by the form on the item view to update an items values.

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -11,4 +11,17 @@ itemRouter.get("/:itemId", async (req, res) => {
 itemRouter.delete("/:itemId", async (req, res) => {
   res.send("item deleted");
 });
+
+itemRouter.post("/updateItem/:itemId", async (req, res) => {
+  let updateResult = await itemController.updateItem(
+    req.params.itemId,
+    req.body.itemName,
+    req.body.itemQuantity,
+    req.body.itemPrice,
+    req.body.itemCategoryIds,
+  );
+  let itemId = req.params.itemId;
+  res.redirect(`/items/${itemId}`);
+});
+
 module.exports = { itemRouter };
