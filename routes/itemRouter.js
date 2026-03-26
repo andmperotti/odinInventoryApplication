1d4e3996af648232c2d3d541d7e309f4e0f08f99
Andrew Perotti <Perotti28@gmail.com>
2026-03-26 12:13:03 -0700

    feat: add createItem route
    fix: move other routes around for readability
    
    In addition for operation purposes these had to be moved around.

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -2,14 +2,25 @@ const { Router } = require("express");
 const itemRouter = Router();
 let itemController = require("../controllers/itemController");
 
+itemRouter.get("/createItem", async (req, res) => {
+  let categories = await itemController.getCategories();
+  res.render("createItem", { categories });
+});
+
 itemRouter.get("/:itemId", async (req, res) => {
   let item = await itemController.getItem(req.params.itemId);
   let categories = await itemController.getCategories();
   res.render("item", { item, categories });
 });
 
-itemRouter.delete("/:itemId", async (req, res) => {
-  res.send("item deleted");
+itemRouter.post("/createItem", async (req, res) => {
+  let creationResult = await itemController.createItem(
+    req.body.itemName,
+    req.body.itemQuantity,
+    req.body.itemPrice,
+    req.body.itemCategoryIds,
+  );
+  res.redirect(`/items/${creationResult.itemId.rows[0].id}`);
 });
 
 itemRouter.post("/updateItem/:itemId", async (req, res) => {
@@ -22,6 +33,10 @@ itemRouter.post("/updateItem/:itemId", async (req, res) => {
   );
   let itemId = req.params.itemId;
   res.redirect(`/items/${itemId}`);
+});
+
+itemRouter.delete("/:itemId", async (req, res) => {
+  res.send("item deleted");
 });
 
 module.exports = { itemRouter };
