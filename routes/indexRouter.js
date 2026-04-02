5c83b6b499b26d41aea46419f60dcd79de95df20
Andrew Perotti <Perotti28@gmail.com>
2026-04-02 09:50:24 -0700

    fix: remove getAllItem method invocation
    This method was used to get a list of all items in the db and we'd display them on the homepage, but with planned changes to the application we're no longer aiming to do that.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,10 +3,7 @@ const indexRouter = Router();
 const dbQueries = require("../db/queries");
 
 indexRouter.get("/", async (req, res) => {
-  //generate links for each category in the db
-  //so query db for categories, also obtaining their id in the table as this will be used to access specific category pages
   let categories = await dbQueries.getCategories();
-  let items = await dbQueries.getAllItems();
-  res.render("homepage", { categories, items });
+  res.render("homepage", { categories });
 });
 module.exports = { indexRouter };
