310b80a46694e791a70d8da5b1d72fcb8ae6a3db
Andrew Perotti <Perotti28@gmail.com>
2026-04-03 12:18:36 -0700

    fix: change what gets sent when requesting a cat
    feat: add create category route
    
    This will also disallow duplicate category creation.

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -2,10 +2,26 @@ const { Router } = require("express");
 const categoryRouter = Router();
 const categoryController = require("../controllers/categoryController");
 
+categoryRouter.get("/create", (req, res) => {
+  res.render("createCategory");
+});
+
 categoryRouter.get("/:categoryId", async (req, res) => {
   let categoryId = req.params.categoryId;
   let categoryReturn = await categoryController.getCategoryItems(categoryId);
-  res.render("category", { ...categoryReturn });
+  res.render("category", { ...categoryReturn, categoryId });
+});
+
+categoryRouter.post("/create", async (req, res) => {
+  let categoryName = req.body.categoryName;
+  let categoryCreationAttempt =
+    await categoryController.createCategory(categoryName);
+
+  if (categoryCreationAttempt !== false) {
+    res.redirect(`/category/${categoryCreationAttempt}`);
+  } else {
+    res.send("Category already exists");
+  }
 });
 
 module.exports = { categoryRouter };
