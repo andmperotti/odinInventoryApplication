662059a4cd8e5e282debb85264b8049b8dde45aa
Andrew Perotti <Perotti28@gmail.com>
2026-04-10 08:00:09 -0700

    fix: switch from connection string to arguments

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,10 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionString: process.env.local_connection_string,
+  // connectionString: process.env.local_connection_string,
+  host: process.env.host,
+  user: process.env.user,
+  database: process.env.database,
+  password: process.env.passwrod,
+  port: process.env.port,
 });
