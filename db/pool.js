b0b55ad02b0f9406079e2c3ee1c07b8cef47c3aa
Andrew Perotti <Perotti28@gmail.com>
2026-04-13 11:48:08 -0700

    fix: change connections to connection

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionsString: process.env.remote_connection_string,
+  connectionString: process.env.remote_connection_string,
   ssl: {
     rejectUnauthorized: false,
   },
